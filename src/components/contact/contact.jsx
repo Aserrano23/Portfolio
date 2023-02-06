5e94b6356d1406c8b448f2f6a6c82f5a72c7c3eb
Adrian Serrano Castillo <adrian@arkerlabs.com>
2023-02-06 13:24:37 +0100

    feat: 👾improved contact form

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -22,57 +22,67 @@ const Contact = () => {
         </div>
       </div>
 
-      <div className="row mt-5">
-        <div className="col">
-          <form
-            action="https://usebasin.com/f/d68a1ce2766e"
-            method="POST"
-            encType="multipart/form-data"
-            id="form"
-          >
-            <div className="row">
-              <div className="col end wow fadeInLeft">
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder={t("name")}
-                  required
-                />
-              </div>
-              <div className="col start wow fadeInRight">
-                <input
-                  type="text"
-                  name="email"
-                  id="email"
-                  placeholder={t("email")}
-                  required
-                />
-              </div>
+      <div className="row">
+        <div className="col-xl-6 col-lg-6 col-md-6 col-12 center-v">
+          <img
+            className="contact-photo"
+            src="/assets/images/adrian.jpg"
+            width="300"
+            alt=""
+          />
+        </div>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-12">
+          <div className="row mt-5">
+            <div className="col">
+              <form
+                action="https://usebasin.com/f/d68a1ce2766e"
+                method="POST"
+                encType="multipart/form-data"
+                id="form"
+              >
+                <div className="row container" style={{ margin: "0" }}>
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 wow fadeInLeft center mt-3">
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      placeholder={t("name")}
+                      required
+                    />
+                  </div>
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 wow fadeInRight center mt-3">
+                    <input
+                      type="text"
+                      name="email"
+                      id="email"
+                      placeholder={t("email")}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="row container mt-3" style={{ margin: "0" }}>
+                  <div className="col-12 center wow fadeInUp">
+                    <textarea
+                      name="message"
+                      id="message"
+                      className="msg-textarea"
+                      placeholder={t("msg")}
+                      required
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="row mt-4">
+                  <div className="col-12 center">
+                    <input
+                      type="submit"
+                      value={t("send")}
+                      className="as-btn submit-form wow fadeIn"
+                    />
+                  </div>
+                </div>
+              </form>
             </div>
-
-            <div className="row mt-3">
-              <div className="col center wow fadeInUp">
-                <textarea
-                  name="message"
-                  id="message"
-                  className="msg-textarea"
-                  placeholder={t("msg")}
-                  required
-                ></textarea>
-              </div>
-            </div>
-
-            <div className="row mt-3">
-              <div className="col center">
-                <input
-                  type="submit"
-                  value={t("send")}
-                  className="as-btn submit-form wow fadeIn"
-                />
-              </div>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
